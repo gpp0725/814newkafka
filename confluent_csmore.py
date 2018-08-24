6fffed9c843f08dc3cb6be9067d5d120b3b597cc
gpp <guopanpan@sinosoft.com.cn>
2018-08-24 14:13:40 +0800

    add  kafka_tool_test.pdf   完善confluent-kafka.py

--- confluent_csmore.py
+++ confluent_csmore.py
@@ -7,30 +7,28 @@ from confluent_kafka import Consumer, KafkaError
 import time
 
 
-def conf_csm(ser_ver, i_d, early, table):
+def conf_csm():
     c = Consumer({
-        'bootstrap.servers': ser_ver,
-        'group.id': i_d,
+        'bootstrap.servers': my_broker,
+        'group.id': ci_d,
         'default.topic.config': {
-            'auto.offset.reset': early,
-            # 'consumer_timeout_ms': float('inf'),
-            # 'session.timeout.ms': 3000
+            'auto.offset.reset': 'earliest',
         }
     })
     c.subscribe([table])
     co_unt = 0
-    tout = 0
-    with open('confluent_kafkamore.csv', 'a')as f_v:
+    # tout = 0
+    with open(path, 'a')as f_v:
         while True:
             msg = c.poll(1.0)
             if msg is None:
-                tout += 1
-                if tout > 10:
-                    break
+                # tout += 1
+                # if tout > 10:
+                #     break
                 continue
             co_unt += 1
-            # if count >= 14069:
-            #     break
+            if co_unt >= 121400:
+                break
             if msg.error():
                 if msg.error().code() == KafkaError._PARTITION_EOF:
                     continue
@@ -47,13 +45,17 @@ def conf_csm(ser_ver, i_d, early, table):
 
 if __name__ == '__main__':
     my_broker = "127.0.0.1:9092"
+    ci_d = 'my_o'
+    table = 'mgj2'
+    path = 'confluent_kafka.csv'
+    save_path = '/tmp/tmp/pycharm_project_4/814new/enhance/save/save1.csv'
     data = []
     with ProcessPoolExecutor(max_workers=2)as t_p:
         start = time.time()
-        data.append(t_p.submit(conf_csm, ser_ver=my_broker, i_d='sui_b', early='earliest', table='hh1', ))
-        data.append(t_p.submit(conf_csm, ser_ver=my_broker, i_d='sui_b', early='earliest', table='hh1', ))
+        data.append(t_p.submit(conf_csm))
+        data.append(t_p.submit(conf_csm))
     t_p.shutdown(wait=True)
-    with open('/tmp/tmp/pycharm_project_4/814new/enhance/save/save1.csv', 'a')as f:
+    with open(save_path, 'a')as f:
         for d in data:
             # if d.done():
             process_id, count, end_time = d.result()
@@ -65,5 +67,3 @@ if __name__ == '__main__':
             f_csv.writerow(row)
             print('process {} finished in {} seconds total {} message.'.format(process_id, end_time - start, count))
     print('main id is {}'.format(os.getpid()))
-
-# no_mal
